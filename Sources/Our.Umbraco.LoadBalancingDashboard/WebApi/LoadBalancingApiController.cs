1e73b7e02d815a5240339217501ea5cea4775c4d
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2016-02-24 08:51:11 +0100

    Added loadbalancing api controller

---
+++ Sources/Our.Umbraco.LoadBalancingDashboard/WebApi/LoadBalancingApiController.cs
@@ -0,0 +1,13 @@
+﻿namespace Our.Umbraco.LoadBalancingDashboard.WebApi
+{
+    using global::Umbraco.Web.Mvc;
+    using global::Umbraco.Web.WebApi;
+
+    /// <summary>
+    /// Load balancing dashboard api controller.
+    /// </summary>
+    [PluginController("Our.Umbraco.LoadBalancingDashboard")]
+    public class LoadBalancingApiController : UmbracoAuthorizedApiController
+    {
+    }
+}
