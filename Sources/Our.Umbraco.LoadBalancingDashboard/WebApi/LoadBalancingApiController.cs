5884a209c3503f1ab98ad73fc146a913ba19b7b1
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2016-02-24 08:56:49 +0100

    Added method to api to get the loadbalancing type

--- Sources/Our.Umbraco.LoadBalancingDashboard/WebApi/LoadBalancingApiController.cs
+++ Sources/Our.Umbraco.LoadBalancingDashboard/WebApi/LoadBalancingApiController.cs
@@ -1,6 +1,13 @@
 ﻿namespace Our.Umbraco.LoadBalancingDashboard.WebApi
 {
+    using System.Web.Http;
+
+    using Our.Umbraco.LoadBalancingDashboard.Enums;
+
+    using global::Umbraco.Core.Configuration;
+
     using global::Umbraco.Web.Mvc;
+
     using global::Umbraco.Web.WebApi;
 
     /// <summary>
@@ -9,5 +16,18 @@
     [PluginController("Our.Umbraco.LoadBalancingDashboard")]
     public class LoadBalancingApiController : UmbracoAuthorizedApiController
     {
+        /// <summary>
+        /// Gets the loadbalancing type
+        /// </summary>
+        /// <returns>
+        /// The <see cref="LoadBalancingType"/>.
+        /// </returns>
+        [HttpGet]
+        public LoadBalancingType GetLoadBalancingType()
+        {
+            return UmbracoConfig.For.UmbracoSettings().DistributedCall.Enabled
+                       ? LoadBalancingType.Traditional
+                       : LoadBalancingType.Flexible;
+        }
     }
 }
