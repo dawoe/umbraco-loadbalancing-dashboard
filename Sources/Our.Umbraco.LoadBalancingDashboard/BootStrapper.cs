6ffbe4b391bf6cf1e7c19f091a77f83dddd685a7
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2016-02-24 08:59:16 +0100

    Added bootstrapper class to handle umbraco application events

---
+++ Sources/Our.Umbraco.LoadBalancingDashboard/BootStrapper.cs
@@ -0,0 +1,11 @@
+﻿namespace Our.Umbraco.LoadBalancingDashboard
+{
+    using global::Umbraco.Core;
+
+    /// <summary>
+    /// Bootstrapper to handle application events
+    /// </summary>
+    internal class BootStrapper : ApplicationEventHandler
+    {
+    }
+}
