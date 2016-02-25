f034e7387766f032b7a16a87bd53721ee9ddbc31
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2016-02-25 16:21:46 +0100

    Added flexible server role model

---
+++ Sources/Our.Umbraco.LoadBalancingDashboard/Models/FlexibleServerInfo.cs
@@ -0,0 +1,48 @@
+﻿namespace Our.Umbraco.LoadBalancingDashboard.Models
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// The flexible server info.
+    /// </summary>
+    [DataContract(Name = "server")]
+    public class FlexibleServerInfo
+    {
+        /// <summary>
+        /// Gets or sets the identity.
+        /// </summary>
+        [DataMember(Name = "identity")]
+        public string Identity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the registered.
+        /// </summary>
+        [DataMember(Name = "registered")]
+        public DateTime Registered { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last accessed.
+        /// </summary>
+        [DataMember(Name = "lastaccessed")]
+        public DateTime LastAccessed { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether is active.
+        /// </summary>
+         [DataMember(Name = "isactive")]
+        public bool IsActive { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address.
+        /// </summary>
+        [DataMember(Name = "address")]
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether is master.
+        /// </summary>
+        [DataMember(Name = "ismaster")]
+        public bool IsMaster { get; set; }
+    }
+}
