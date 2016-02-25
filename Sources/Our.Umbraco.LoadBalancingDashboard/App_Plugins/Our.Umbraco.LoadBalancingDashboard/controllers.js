b5509ca50326684857fee800ce8b5e24ac4789a2
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2016-02-25 16:32:56 +0100

    Added seperate view for showing flexible loadbalancing information

--- Sources/Our.Umbraco.LoadBalancingDashboard/App_Plugins/Our.Umbraco.LoadBalancingDashboard/controllers.js
+++ Sources/Our.Umbraco.LoadBalancingDashboard/App_Plugins/Our.Umbraco.LoadBalancingDashboard/controllers.js
@@ -12,3 +12,16 @@
         $scope.loadData();
     }
 ]);
+
+angular.module("umbraco").controller("Our.Umbraco.LoadBalancindDashboard.FlexibleController", [
+    '$scope',
+    'Our.Umbraco.LoadBalancindDashboard.LoadBalancingApiResource',
+    function ($scope, loadBalancingApiResource) {
+
+        $scope.loadData = function () {
+           
+        }
+
+        $scope.loadData();
+    }
+]);
